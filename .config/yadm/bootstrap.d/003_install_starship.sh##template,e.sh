a873ce277f3b0adf6b0354d89c2b1acf31be6471
Nino Maruszewski <nino.maruszewski@gmail.com>
2022-09-05 11:02:23 -0500

    bootstrap: use right dir for starship

--- .config/yadm/bootstrap.d/003_install_starship.sh##template,e.sh
+++ .config/yadm/bootstrap.d/003_install_starship.sh##template,e.sh
@@ -12,15 +12,16 @@ echo "Installing starship!"
 # Download executable to $HOME/.local/bin
 local_bin="$HOME/.local/bin"
 mkdir -p "$local_bin"
+cd "$local_bin"
 
 starship_zip_download_url=$(
     curl -sL https://api.github.com/repos/starship/starship/releases/latest |
         jq -r ".assets[].browser_download_url" |
         grep "starship-x86_64-pc-windows-msvc.zip$"
 )
-curl -fsL "$starship_zip_download_url" -o "$local_bin/starship.zip" &&
-    unzip -u "$local_bin/starship.zip" &&
-    rm "$local_bin/starship.zip"
+curl -fsL "$starship_zip_download_url" -o "starship.zip" &&
+    unzip -u "$starship.zip" &&
+    rm "starship.zip"
 
 {% else %}
 
