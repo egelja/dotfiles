1f0469ae0a94b91c19727920c1e21ef59623989a
Nino Maruszewski <nino.maruszewski@gmail.com>
2022-09-07 15:41:50 -0500

    powershell: add and fix aliases

--- Documents/WindowsPowerShell/profile.ps1##os.Msys,e.ps1
+++ Documents/WindowsPowerShell/profile.ps1##os.Msys,e.ps1
@@ -1,15 +1,13 @@
 Set-Alias nano C:\msys64\usr\bin\nano.exe
 
-function Tree-AllFiles { tree.com /f $args }
-Set-Alias tree Tree-AllFiles -Force
-
-function Get-AllChildItems { Get-ChildItem -Force $args }
-Set-Alias la Get-AllChildItems -Force
+function tree { tree.com /f @args }
+function la { Get-ChildItem -Force @args }
 
 Set-Alias c Clear-Host
 Set-Alias ipy "ipython.exe"
 Set-Alias make "mingw32-make.exe"
 # Set-Alias haste "C:\src\WinHaste\WinHaste.exe"
+Set-Alias time Measure-Command
 
 $PowerShellProfile = $PROFILE.CurrentUserAllHosts
 $PowerShellPath = Split-Path $PowerShellProfile
