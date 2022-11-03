b96c3ed03a4fd1e9388cfd34d28428263f90fbb3
Nikola Maruszewski <nikolamaruszewski2026@u.northwestern.edu>
2022-11-02 23:14:28 -0500

    bash: cd aliases

--- .bash_aliases##template,e.sh
+++ .bash_aliases##template,e.sh
@@ -199,3 +199,47 @@ mktar() {
 
     command tar -cv$compress_arg -f "$dir_name.tar.$ext" $dir_name
 }
+
+
+#
+# CD
+#
+# https://mhoffman.github.io/2015/05/21/how-to-navigate-directories-with-the-shell.html
+function cd() {
+    if [ "$#" = "0" ]
+    then
+        pushd ${HOME} > /dev/null
+    elif [ -f "${1}" ]
+    then
+        ${EDITOR:-nano} ${1}
+    else
+        pushd "$1" > /dev/null
+    fi
+}
+
+function bd() {
+    if [ "$#" = "0" ]
+    then
+        popd > /dev/null
+    else
+        for i in $(seq ${1})
+        do
+            popd > /dev/null
+        done
+    fi
+}
+
+# Back aliases
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+alias ......="cd ../../../../.."
+alias .......="cd ../../../../../.."
+alias ........="cd ../../../../../../.."
+alias .........="cd ../../../../../../../.."
+alias ..........="cd ../../../../../../../../.."
+alias ...........="cd ../../../../../../../../../.."
+alias ............="cd ../../../../../../../../../../.."
+alias .............="cd ../../../../../../../../../../../.."
+
