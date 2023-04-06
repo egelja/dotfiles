d603236eb952331fddebd07754c8af49cb36e744
Nino Maruszewski <nino.maruszewski@gmail.com>
2023-04-06 12:56:03 -0500

    more powershell thigns

--- Documents/PowerShell/profile.ps1##os.Msys,e.ps1
+++ Documents/PowerShell/profile.ps1##os.Msys,e.ps1
@@ -7,14 +7,8 @@ function which { (Get-Command @args).Source }
 Remove-Alias -Force sl
 function sl { sl.exe -ad5w @args }
 
-function startx {
-    xlaunch.exe
-    $ENV:DISPLAY = "localhost:0.0"
-}
-
 Set-Alias c Clear-Host
 Set-Alias ipy "ipython.exe"
-Set-Alias make "mingw32-make.exe"
 # Set-Alias haste "C:\src\WinHaste\WinHaste.exe"
 
 Set-Alias -Force cat "bat.exe"
@@ -24,8 +18,39 @@ Set-Alias less "C:\msys64\usr\bin\less.exe"
 # VirtualEnvWrapper
 $PSConfigDir = Split-Path $PROFILE.CurrentUserAllHosts
 $VenvWrapperScript = "$PSConfigDir\Modules\VirtualEnvWrapper.psm1"
+
 if (![System.IO.File]::Exists($VenvWrapperScript)) {
     $VenvWrapperScriptDownload = "https://raw.githubusercontent.com/regisf/virtualenvwrapper-powershell/master/VirtualEnvWrapper.psm1"
     Invoke-WebRequest -Uri $VenvWrapperScriptDownload -OutFile $VenvWrapperScript
 }
+
 Import-Module $VenvWrapperScript
+
+# VcXSrv
+if (Get-Process VcxSrv -ErrorAction SilentlyContinue) {
+    $ENV:DISPLAY = "localhost:0.0"
+}
+
+function startx {
+    if (!$(Get-Process VcxSrv -ErrorAction SilentlyContinue)) {
+        xlaunch.exe
+    }
+    $ENV:DISPLAY = "localhost:0.0"
+}
+
+function stopx {
+    $srv = Get-Process VcxSrv -ErrorAction SilentlyContinue
+    
+    if ($srv) {
+        # Try gracefully first
+        $srv | Stop-Process
+
+        # kill after five seconds
+        Sleep 5
+        if (!$srv.HasExited) {
+            $srv | Stop-Process -Force
+        }
+    }
+
+    Remove-Variable srv
+}
