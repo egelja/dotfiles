9694fe6993240f25ed713ea40f8ea9f23c13d55b
Nikola Maruszewski <nikolamaruszewski2026@u.northwestern.edu>
2023-09-05 11:56:41 -0500

    small bash fixes

--- .bashrc.local##h.dubliner,e.sh
+++ .bashrc.local##h.dubliner,e.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+# Fix poetry and venvwrapper
 export VIRTUALENVWRAPPER_PYTHON=`which python3.11`
 export PYTHON_KEYRING_BACKEND=keyring.backends.null.Keyring
+
+# Gurobi
+export GUROBI_HOME="/opt/gurobi/linux64"
+export PATH="${PATH}:${GUROBI_HOME}/bin"
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${GUROBI_HOME}/lib"
